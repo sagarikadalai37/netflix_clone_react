52f01c7900bb8db64dad9ff5dea9e6d3d032f5b6
sagarikadalai37 <96329273+sagarikadalai37@users.noreply.github.com>
2023-02-06 12:41:17 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      © NETFLIX Clone made by Sagarika
+    </footer>
+  );
+}
+
+export default Footer;
